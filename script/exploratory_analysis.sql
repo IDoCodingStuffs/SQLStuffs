232745bbf8223e35ef8afc0af21a497d3ab32dc2
IDoCodingStuffs <sahinvictor@gmail.com>
2019-01-31 23:11:47 -0500

    Exploratory analysis complete

--- script/exploratory_analysis.sql
+++ script/exploratory_analysis.sql
@@ -49,9 +49,6 @@ ORDER BY count DESC;
 -- 3) Analysis for courses
 
 SELECT TOP 5 * FROM courses; --Table head. Cols: uuid, name, number
--- Shared key uuid with sections and course_offerings table
--- uuid possibly the course key?
-
 SELECT COUNT(*) FROM courses; --Total rows: 9306
 
 SELECT name, COUNT(name) AS count --Count of each unique course name
@@ -63,11 +60,93 @@ ORDER BY count DESC;
 SELECT name, number --Count of each unique course-number combo
 FROM courses -- 8522 Unique combinations
 GROUP BY name, number; -- Seems like same uuid can be assigned to the same course multiple times
--- Maybe uuid is something else?
+
+-- Shared key uuid with sections and course_offerings table
+-- uuid possibly the course key?
+-- Maybe something else?
 
 --------------------------
 -- 4) Analysis for grade_distribution
 
-SELECT TOP 5 * FROM grade_distributions; --Table head. Cols: course_offering_uuid, section numbers, count per grade (column per each grade), 
-
+SELECT TOP 5 * FROM grade_distributions; --Table head. Cols: course_offering_uuid, section numbers, count per grade (column per each grade) 
 SELECT COUNT(*) FROM grade_distributions; --Total rows: 193262
+
+-- This is likely aggregated from student data, which is likely not included because sensitive info and all that.
+-- Further analysis outside scope of exploration
+
+------------------------------------
+-- 5) Analysis for instructors
+
+SELECT TOP 5 * FROM instructors; --Table head. Cols: id, name
+SELECT COUNT(*) FROM instructors; --Total rows: 18737
+
+-- This is just a reference table to look up instructor names from id
+
+----------------------------------------
+-- 6) Analysis for rooms
+
+SELECT TOP 5 * FROM rooms; --Table head. Cols: uuid, facility_code, room_code
+SELECT COUNT(*) FROM rooms; --Total rows: 1350
+
+SELECT facility_code, COUNT(facility_code) AS count --Count of each unique facility code
+FROM rooms -- 132 unique facility codes
+GROUP BY facility_code -- Likely for administrative convenience, although ONLINE course info contained
+ORDER BY count DESC;
+
+--------------------------------------
+-- 7) Analysis for schedules
+
+SELECT TOP 5 * FROM schedules; --Table head. Cols: uuid, start_time, end_time, col per day of week
+SELECT COUNT(*) FROM schedules; --Total rows: 4467
+
+SELECT start_time FROM schedules -- All unique start times
+GROUP BY start_time -- The time notations do not fit hour notations
+ORDER BY start_time DESC; -- Although first two fit 24 hour notations, last 2 likely code
+
+SELECT end_time FROM schedules -- All unique end times
+GROUP BY end_time -- The time notations do not fit hour notations either, although make sense for mins
+ORDER BY end_time DESC; -- Possibly coded as (hour x 60 + minute) to denote hour and minute 
+
+SELECT start_time, COUNT(start_time) AS count -- Count of each unique start time
+FROM schedules -- In descending order
+GROUP BY start_time -- The expectation is that the most popular times will make sense for a college
+ORDER BY count DESC; -- For the (hour x 60 + minute theory)
+
+SELECT end_time, COUNT(end_time) AS count -- Count of each unique start time
+FROM schedules -- In descending order
+GROUP BY end_time -- The expectation is that the most popular times will make sense for a college
+ORDER BY count DESC; -- For the (hour x 60 + minute theory)
+
+-- If the theory is correct, the most popular 3 start dates would be: 13:20, 14:25 and 11:00
+-- The most popular end times would be: 17:00, 10:45 and 16:00
+-- Most likely makes sense, can be used as the logic for data cleaning
+
+-- 8) Analysis for subject_memberships
+
+SELECT TOP 5 * FROM subject_memberships; --Table head. Cols: subject_code, course_offering_uuid
+SELECT COUNT(*) FROM subject_memberships; --Total rows: 95314
+
+-- Most likely a lookup table. Given how both cols are keys. Likely between course_offerings and subjects
+
+-------------------------------------------------------
+-- 9) Analysis for subjects
+
+SELECT TOP 5 * FROM subjects; --Table head. Cols: code, name, abbreviation
+SELECT COUNT(*) FROM subjects; --Total rows: 200
+
+-- Subject reference table. Code is shared key linked with subject_memberships which is linked with course_offerings
+
+----------------------------------------------------------
+-- 10) Analysis for teachings
+
+SELECT TOP 5 * FROM teachings; --Table head. Cols: instructor_id, section_uuid
+SELECT COUNT(*) FROM teachings; --Total rows: 315211
+
+-- Likely reference table between instructors and sections
+
+------------------------------------------------------
+
+/*
+With this information we can generate database schema as well as a simple report explaining what is 
+going on with the dataset
+*/
